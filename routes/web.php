c5380e9be0e4feac95e46fb5ec41e7f9d82283b6
brendinhasantana <brendhasantanabss12@gmail.com>
2023-04-06 01:31:15 -0300

    Changes to be committed: 	new file:   routes/web.php
    Untracked files:
    	.editorconfig
    	.env.example
    	.gitattributes
    	.gitignore
    	.styleci.yml
    	README.md
    	app/
    	artisan
    	bootstrap/
    	composer.json
    	composer.lock
    	config/
    	database/
    	package.json
    	phpunit.xml
    	public/
    	resources/
    	routes/api.php
    	routes/channels.php
    	routes/console.php
    	server.php
    	storage/
    	tests/
    	webpack.mix.js

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Echo_;
+use App\Http\Controllers\EventController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hi/{nome}', [EventController::class, 'name'])->name('hi');
+
+
+Route::get('/contas/{numero1}/{numero2}/{operacao?}', [EventController::class, 'contas'])->name('contas');
+
+Route::get('/conta/{numero1}/{numero2}/{operacao?}', function ($numero1, $numero2, $operacao = '') {
+    switch ($operacao) {
+        case "soma":
+            $resultado = $numero1 + $numero2;
+            echo 'Soma : ' . $resultado;
+            break;
+        case 'subtração':
+            $resultado = $numero1 - $numero2;
+            echo 'Subtração :' . $resultado;
+            break;
+        case 'multiplicação':
+            $resultado = $numero1 * $numero2;
+            echo 'Multiplicação: ' . $resultado;
+            break;
+        case 'divisão':
+            $resultado = $numero1 / $numero2;
+            echo 'Divisão: ' . $resultado;
+            break;
+        case '':
+            $resultado = $numero1 + $numero2;
+            echo 'Soma: ' . $resultado;
+            $resultado = $numero1 - $numero2;
+            echo '</br>Subtração: ' . $resultado;
+            $resultado = $numero1 * $numero2;
+            echo '</br>Multiplicação: ' . $resultado;
+            $resultado = $numero1 / $numero2;
+            echo '</br>Divisão: ' . $resultado;
+            break;
+    };
+})->name('operations');
+//php artisan serve
+
+Route::get('/years/{ano}/{mes?}/{dia?}',[EventController::class, 'years'])->name('years');
+
+Route::get('/idade/{ano}/{mes?}/{dia?}', function (int $ano, int $mes=0, int $dia=0) {
+
+    $entrada = new DateTime('now');
+    $saida = new DateTime("$ano-$mes-$dia");
+    $idade = $entrada->diff($saida);
+
+    if ($entrada->format('Y') >= $saida->format('Y')){
+        echo 'Insira uma idade valida';
+    }
+    elseif ($dia==0 && $mes!=0 ){
+        echo 'Você tem ' . $idade->y . ' anos ' . $idade->m . ' meses';
+    }
+    elseif ($mes==0){
+        echo 'Você tem ' . $idade->y . ' anos';
+    }
+    else{
+        echo 'Você tem ' . $idade->y . ' anos ' . $idade->m . ' meses e ' . $idade->d . ' dias';
+    }
+
+
+})->where(['ano'=>'[0-9]{4}', 'mes'=>'[0-9]{1,2}', 'dia'=>'[0-9]{1,2}']) ; 
+
